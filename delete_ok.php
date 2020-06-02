021673308dbfba0e8ef055e03bca6382df6b21ce
leemk0329 <65477956+leemk0329@users.noreply.github.com>
2020-06-02 23:25:02 +0900

    본문삭제시 댓글 함께 삭제처리
    본문 삭제시 페이지에는 댓글이 없어지나 DB에는 남아있어 함께 삭제할수 있도록 수정

--- delete_ok.php
+++ delete_ok.php
@@ -3,5 +3,8 @@ require("db.php");
 $sql = mysqli_connect($AD,$ID,$PW,$DB);
 $select_query = "DELETE FROM board WHERE ID={$_POST["ID"]}";
 $result = mysqli_query($sql,$select_query);
+// 본문삭제시 댓글삭제 CO_NUM과 본문 ID값이 같음
+$select_query2 = "DELETE FROM comment WHERE CO_NUM={$_POST["ID"]}";
+$result2 = mysqli_query($sql,$select_query2);
 header('Location:/index.php?');
 ?>
