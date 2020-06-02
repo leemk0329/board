daddc472be8b029568325f8ce53bedc11f2eedf1
leemk0329 <65477956+leemk0329@users.noreply.github.com>
2020-06-02 20:49:47 +0900

    Delete insert.php

--- insert.php
+++
@@ -1,8 +0,0 @@
-<?php
-$sql = mysqli_connect("localhost","root","lmk189600","project");
-$select_query = "INSERT INTO board(Title, Maintext, NAME, Date)
-                VALUES('테스트8','테스트8','이명경',NOW())";
-$result = mysqli_query($sql,$select_query);
-
-
- ?>
