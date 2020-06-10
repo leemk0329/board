07eb6cb3c00341baa86baf6c7b3d70fad53775e3
leemk0329 <65477956+leemk0329@users.noreply.github.com>
2020-06-10 09:35:43 +0900

    게시글 조회수 수정
    게시글 조회수 카운트 수정..

--- create_ok.php
+++ create_ok.php
@@ -1,11 +1,14 @@
 <?php
 session_start();
+echo $_POST["Title"];
+echo $_POST["Maintext"];
+echo $_SESSION["USER_ID"];
 require("db.php");
 $sql = mysqli_connect($AD,$ID,$PW,$DB);
 $re_num ="ALTER TABLE board AUTO_INCREMENT=1";
 $renum = mysqli_query($sql,$re_num);
-$select_query = "INSERT INTO board(Title, Maintext, NAME, Date)
-                VALUES('{$_POST["Title"]}','{$_POST["Maintext"]}','{$_SESSION["USER_ID"]}',NOW())";
+$select_query = "INSERT INTO board(Title, Maintext, NAME, Date, HIT)
+                VALUES('{$_POST["Title"]}','{$_POST["Maintext"]}','{$_SESSION["USER_ID"]}',NOW(),1)";
 $result = mysqli_query($sql,$select_query);
 if($result === false){
   echo '작성에 실패하였습니다';
